3e7d3c8b1a9dd122302268089fc84edfb1f7f234
zcm <2601973667@qq.com>
2022-01-26 22:12:19 +0800

    补充协议声明

--- MinecraftLevelTools/MinecraftNBTLibrary/NBTNodeCompound.cs
+++ MinecraftLevelTools/MinecraftNBTLibrary/NBTNodeCompound.cs
@@ -4,6 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+This file is part of MinecraftLevelTools.
+
+This product is unofficial and not from Minecraft or approved by Minecraft.
+
+Copyright (C) 2022  ZCM
+
+This program is released under license AGPL-3.0-only.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License, Version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 namespace MinecraftNBTLibrary
 {
     public class NBTNodeCompound : NBTNodeDataCollection<NBTNode>
